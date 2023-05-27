682cc9c5825f79930c985c27169adcfcc0b21e10
K-Vardhan <kritivardhan020@gmail.com>
2023-05-27 23:54:47 +0530

    Header Section...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className="h-wrapper">
+      <div className="h-container">
+
+        <img src="./logo.png" alt="logo" width={100} />
+
+        <div className="h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contant Us</a>
+          <a href="">Get Started</a>
+          <button className='button'>
+          <a href="">Contant</a>
+          </button>
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
