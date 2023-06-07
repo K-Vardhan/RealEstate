13ee593ffd3fd0b3fc678e913c22ef0020e73619
K-Vardhan <kritivardhan020@gmail.com>
2023-06-07 23:59:36 +0530

    Left Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,12 +7,20 @@ const Hero = () => {
       <div className="paddings innerWidth flexCenter hero-container">
 
         <div className="hero-left">
-          left Section
+
+          <div className="hero-title">
+            <h1>Discover <br /> Most Suitable <br /> Property</h1>
+          </div>
+
+          <div className="hero-des">
+            <span></span>
+          </div>
+
         </div>
 
         <div className="flexCenter hero-right">
           <div className="image-container">
-           <img src="./hero-image.png" alt="" />
+            <img src="./hero-image.png" alt="" />
           </div>
         </div>
 
