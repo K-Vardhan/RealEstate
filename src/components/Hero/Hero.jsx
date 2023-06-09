6b4b4fa053f5f24d79ae12e43e7b40567343313f
K-Vardhan <kritivardhan020@gmail.com>
2023-06-09 23:59:45 +0530

    add left side section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import "./Hero.css"
+import "./Hero.css";
+import {HiLocationMarker} from 'react-icons/hi'
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container">
 
-        <div className="hero-left">
+        <div className="flexColStart hero-left">
 
           <div className="hero-title">
+            <div className="orange-circle"/>
             <h1>Discover <br /> Most Suitable <br /> Property</h1>
           </div>
 
-          <div className="hero-des">
-            <span></span>
+          <div className="flexColStart hero-des">
+            <span>Find a variety of properties that suit you easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className="search-bar">
+           <HiLocationMarker color="var(--blue)" size={25}/>
+           <input type="text" />
+           <button className='button'>Search</button>
           </div>
 
         </div>
