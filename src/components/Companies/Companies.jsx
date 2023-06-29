2c2cf04ce5ec0f6812aa876ba72d29bc90baf06c
K-Vardhan <kritivardhan020@gmail.com>
2023-06-30 00:05:43 +0530

    feat - done companies section

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Companies.css'
+import './Companies.css';
 
 const Companies = () => {
   return (
@@ -8,6 +8,7 @@ const Companies = () => {
             <img src="./prologis.png" alt="" />
             <img src="./tower.png" alt="" />
             <img src="./equinix.png" alt="" />
+            <img src="./realty.png" alt="" />
         </div>
     </section>
   )
