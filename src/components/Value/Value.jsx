d80964cdfed3ec8d2b07a9b11a3c9fa41c75fa54
K-Vardhan <kritivardhan020@gmail.com>
2023-07-12 23:57:48 +0530

    feat & styl - Value.

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import {
-    Accordion,
-    AccordionItem,
-    AccordionItemHeading,
-    AccordionItemButton,AccordionItemPanel,
-    AccordionItemState
- } from 'react-accessible-accordion'
- import "react-accessible-accordion/dist/fancy-example.css";
- import {MdOutlineArrowDropDown} from 'react-icons/md';
- import './Value.css'
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton, AccordionItemPanel,
+  AccordionItemState
+} from 'react-accessible-accordion'
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from 'react-icons/md';
+import './Value.css'
 
 
 const Value = () => {
   return (
-    <div>Value</div>
+    <section className="v-wrapper">
+      <div className="paddings innerWidth flexCenter v-contaier">
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.png" alt="" />
+          </div>
+        </div>
+
+        <div className="flexColStart v-right">
+          <span className='orangeText'>Our Value</span>
+          <span className='primaryText'>Value We Give to You</span>
+          <span className='secondaryText'>We always ready to help by provinding the best services for You <br />
+            we Beleive a good place to live can make your zindgi better.
+          </span>
+        </div>
+      </div>
+    </section>
   )
 }
 
