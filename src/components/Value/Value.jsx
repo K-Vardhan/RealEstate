32009f88436cfc9c48e19aad9d43aa3bceced0d5
K-Vardhan <kritivardhan020@gmail.com>
2023-07-13 00:38:18 +0530

    Style- Everything related to styling

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Accordion,
   AccordionItem,
@@ -9,6 +9,7 @@ import {
 import "react-accessible-accordion/dist/fancy-example.css";
 import { MdOutlineArrowDropDown } from 'react-icons/md';
 import './Value.css'
+import data from '../../utils/accordion'
 
 
 const Value = () => {
@@ -27,6 +28,51 @@ const Value = () => {
           <span className='secondaryText'>We always ready to help by provinding the best services for You <br />
             we Beleive a good place to live can make your zindgi better.
           </span>
+
+
+          <Accordion className='accordion'
+            allowMultipleExpanded={false}
+            preExpanded={[0]}
+          >
+            {
+              data.map((item, i) => {
+                const [className, setClassName] = useState(null)
+                return (
+                  <AccordionItem className={`accordionItem ${className}`} key={i} uuid={i}>
+
+                    <AccordionItemHeading>
+                      <AccordionItemButton className='flexCenter accordionButton'>
+                        <AccordionItemState>
+                          {({ expanded }) => 
+                          expanded ? setClassName("expanded") : setClassName("collapsed")}
+                        </AccordionItemState>
+
+                        <div className="flexCenter icon">
+                          {item.icon}
+                        </div>
+                        <span className="primaryText">
+                          {item.heading}
+                        </span>
+                        <div className="flexCenter icon">
+                          <MdOutlineArrowDropDown size={20} />
+                        </div>
+                      </AccordionItemButton>
+
+                    </AccordionItemHeading>
+
+                    <AccordionItemPanel>
+                      <p className="secondaryText">
+                        {item.detail}
+                      </p>
+                    </AccordionItemPanel>
+
+                  </AccordionItem>
+                )
+              })
+            }
+
+          </Accordion>
+
         </div>
       </div>
     </section>
