b57357efa327820604ffcba833fd25701f1d43c4
K-Vardhan <kritivardhan020@gmail.com>
2024-01-03 23:36:01 +0530

    Add responsiveness to all screens.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,75 @@
-import React from 'react'
-import './Header.css'
+import React, { useState } from "react";
+import "./Header.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import { getMenuStyles } from "../../utils/common";
+import useHeaderColor from "../../hooks/useHeaderColor";
+import OutsideClickHandler from "react-outside-click-handler";
+import { Link, NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import ProfileMenu from "../ProfileMenu/ProfileMenu";
+import AddPropertyModal from "../AddPropertyModal/AddPropertyModal";
+import useAuthCheck from "../../hooks/useAuthCheck.jsx";
 
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const headerColor = useHeaderColor();
+  const [modalOpened, setModalOpened] = useState(false);
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
+  const { validateLogin } = useAuthCheck();
+
+
+  const handleAddPropertyClick = () => {
+    if (validateLogin()) {
+      setModalOpened(true);
+    }
+  };
   return (
-    <section className="h-wrapper">
-      <div className="flexCenter paddings innerWidth h-container">
+    <section className="h-wrapper" style={{ background: headerColor }}>
+      <div className="flexCenter innerWidth paddings h-container">
+        {/* logo */}
+        <Link to="/">
+          <img src="./logo.png" alt="logo" width={100} />
+        </Link>
 
-        <img src="./logo.png" alt="logo" width={100} />
+        {/* menu */}
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setMenuOpened(false);
+          }}
+        >
+          <div
+            // ref={menuRef}
+            className="flexCenter h-menu"
+            style={getMenuStyles(menuOpened)}
+          >
+            <NavLink to="/properties">Properties</NavLink>
 
-        <div className="flexCenter h-menu">
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
-          <a href="">Contant Us</a>
-          <a href="">Get Started</a>
-          <button className='button'>
-          <a href="">Contant</a>
-          </button>
-          
+            <a href="mailto:zainkeepscode@gmail.com">Contact</a>
+
+            {/* add property */}
+            <div onClick={handleAddPropertyClick}>Add Property</div>
+            <AddPropertyModal opened={modalOpened} setOpened={setModalOpened} />
+            {/* login button */}
+            {!isAuthenticated ? (
+              <button className="button" onClick={loginWithRedirect}>
+                Login
+              </button>
+            ) : (
+              <ProfileMenu user={user} logout={logout} />
+            )}
+          </div>
+        </OutsideClickHandler>
+
+        {/* for medium and small screens */}
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
